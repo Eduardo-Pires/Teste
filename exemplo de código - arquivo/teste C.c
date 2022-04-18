d8af91952fd022c7639b084c0529e850247341a4
Eduardo Pires <duduspiress@hotmail.com>
2022-04-17 22:08:42 -0300

    mudança de nome de pasta e gitignore
    mudei o nome da pasta para algo mais abrangente e também testei a funcionalidade de .gitignore após o repositório já estar criado

---
+++ exemplo de código - arquivo/teste C.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main()
+{
+    //apenas um teste de .gitignore após o arquivo ter sido criado;
+    //como eu utilizo o Code::Blocks para codar em C, arquivos .exe e .o sempre são gerados para executar o código;
+
+    printf("Ola, mundo!\n");
+
+return 0;
+}
